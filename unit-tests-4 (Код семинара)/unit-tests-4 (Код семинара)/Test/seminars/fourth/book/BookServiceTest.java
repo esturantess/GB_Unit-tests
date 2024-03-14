8caddb81962f504da56c4248374124bb2c1d75ad
Polina <ms.maylin@inbox.ru>
2024-03-14 03:24:19 +0300

    BookServiceTest has been updated

--- unit-tests-4 (Код семинара)/unit-tests-4 (Код семинара)/Test/seminars/fourth/book/BookServiceTest.java
+++ unit-tests-4 (Код семинара)/unit-tests-4 (Код семинара)/Test/seminars/fourth/book/BookServiceTest.java
@@ -2,6 +2,7 @@ package seminars.fourth.book;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 import java.util.Arrays;
 
@@ -11,17 +12,24 @@ import static org.mockito.Mockito.*;
 
 class BookServiceTest {
 
-    @Test
-    void bookServiceTest() {
+    @Mock
+    private BookRepository bookRepositoryMock = mock(BookRepository.class);
+    BookService bookService = new BookService(bookRepositoryMock);
 
-        BookRepository bookRepositoryMock = mock(BookRepository.class);
-        BookService bookService = new BookService(bookRepositoryMock);
+    @Test
+    void bookServiceFindBookByIdTest() {
 
         when(bookService.findBookById("1")).thenReturn(new Book("1", "Title", "Author"));
-        when(bookService.findAllBooks()).thenReturn(Arrays.asList(new Book("1"), new Book("2"), new Book("3")));
 
         assertEquals("1", bookService.findBookById("1").getId());
         verify(bookRepositoryMock, times(1)).findById("1");
+
+    }
+
+    @Test
+    void bookServiceFindAllBooksTest() {
+
+        when(bookService.findAllBooks()).thenReturn(Arrays.asList(new Book("1"), new Book("2"), new Book("3")));
 
         assertEquals(3, bookRepositoryMock.findAll().size());
         verify(bookRepositoryMock, times(1)).findAll();
